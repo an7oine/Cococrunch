f42cd5324b292a64df6cbfc402809ea8c32f96e8
Antti Hautaniemi <antti@VMware.local>
2015-05-13 09:41:44 +0300

    Configuration updates

--- Cococrunch/PopoverCrunchingVC.m
+++ Cococrunch/PopoverCrunchingVC.m
@@ -10,9 +10,6 @@
 @end
 
 @implementation PopoverCrunchingVC
-{
-	UIPopoverController *_popoverC;
-}
 
 - (void)viewDidLoad
 {
@@ -27,11 +24,11 @@
 - (void)gotTapGesture:(UITapGestureRecognizer *)sender
 {
 	UITableViewController *tableVC = [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
-	_popoverC = [[self.classSwitch.on? NSClassFromString(@"PhonePopoverController") : UIPopoverController.class alloc] initWithContentViewController:tableVC];
-	_popoverC.delegate = self;
+	UIPopoverController *popoverC = [[self.classSwitch.on? NSClassFromString(@"PhonePopoverController") : UIPopoverController.class alloc] initWithContentViewController:tableVC];
+	popoverC.delegate = self;
 
 	CGRect frame = CGRectInset((CGRect){ [sender locationInView:self.view], CGSizeZero }, -1.0f, -1.0f);
-	[_popoverC presentPopoverFromRect:frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+	[popoverC presentPopoverFromRect:frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
