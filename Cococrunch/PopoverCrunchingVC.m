b2ae8d308c716e07bc5fb30bfa5856575373115e
antti <antti@Mavericks.local>
2016-02-20 11:41:15 +0200

    Crunching dispatch tables

--- Cococrunch/PopoverCrunchingVC.m
+++ Cococrunch/PopoverCrunchingVC.m
@@ -5,6 +5,7 @@
 #import "PopoverCrunchingVC.h"
 
 #import "UIPopoverController+UniversalSupport.h"
+#import "DispatchTableViewController.h"
 
 @interface PopoverCrunchingVC () <UIPopoverControllerDelegate, UITextFieldDelegate>
 @end
@@ -26,7 +27,17 @@
 
 - (void)gotTapGesture:(UITapGestureRecognizer *)sender
 {
-	UITableViewController *tableVC = [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
+	DispatchTableViewController *tableVC = [[DispatchTableViewController alloc] initWithStyle:UITableViewStyleGrouped];
+    tableVC.cellStyle = UITableViewCellStyleValue1;
+    [tableVC setHeader:@"Header" forSection:0];
+    [tableVC addChoiceIntoSection:0 withTitle:@"First row" detail:@"detail" accessoryType:UITableViewCellAccessoryCheckmark backgroundColour:nil block:^(NSIndexPath *indexPath) {
+        NSLog(@"1st: indexPath %@", indexPath);
+    }];
+    [tableVC addChoiceIntoSection:0 withTitle:@"Second row" detail:@"detail" accessoryType:UITableViewCellAccessoryCheckmark backgroundColour:nil block:^(NSIndexPath *indexPath) {
+        NSLog(@"2nd: indexPath %@", indexPath);
+    }];
+    [tableVC setFooter:@"Footer" forSection:0];
+  
 	UIPopoverController *popoverC = [[self.classSwitch.on? NSClassFromString(@"PhonePopoverController") : UIPopoverController.class alloc] initWithContentViewController:tableVC];
 	popoverC.delegate = self;
 
